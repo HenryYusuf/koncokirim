2b1450a58c2929da1cd99ebb9bcf0d14243d7f48
henryyusuf <baharhenry10@gmail.com>
2025-05-19 16:57:41 +0700

    feat: user page ads feature

--- resources/views/frontend/layouts/ads.blade.php
+++ resources/views/frontend/layouts/ads.blade.php
@@ -1,30 +1,19 @@
+@php
+    $banners = App\Models\Banner::latest()->limit(4)->get();
+@endphp
+
 <section class="section pt-5 pb-5 bg-white homepage-add-section">
     <div class="container">
         <div class="row">
-            <div class="col-md-3 col-6">
-                <div class="products-box">
-                    <a href="listing.html"><img alt="" src="{{asset('frontend/img/pro1.jpg')}}"
-                            class="img-fluid rounded"></a>
+
+            @foreach ($banners as $key => $item)
+                <div class="col-md-3 col-6">
+                    <div class="products-box">
+                        <a href="{{ $item->url }}"><img alt="" src="{{ $item->image }}" class="img-fluid rounded"></a>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-3 col-6">
-                <div class="products-box">
-                    <a href="listing.html"><img alt="" src="{{asset('frontend/img/pro2.jpg')}}"
-                            class="img-fluid rounded"></a>
-                </div>
-            </div>
-            <div class="col-md-3 col-6">
-                <div class="products-box">
-                    <a href="listing.html"><img alt="" src="{{asset('frontend/img/pro3.jpg')}}"
-                            class="img-fluid rounded"></a>
-                </div>
-            </div>
-            <div class="col-md-3 col-6">
-                <div class="products-box">
-                    <a href="listing.html"><img alt="" src="{{asset('frontend/img/pro4.jpg')}}"
-                            class="img-fluid rounded"></a>
-                </div>
-            </div>
+            @endforeach
+
         </div>
     </div>
 </section>
