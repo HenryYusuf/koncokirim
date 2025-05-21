47a8210ac5c233b82938c0592719920ee623ef20
henryyusuf <baharhenry10@gmail.com>
2025-05-21 18:10:28 +0700

    changes: fix bug layout id when not logged in

--- resources/views/frontend/dashboard/header.blade.php
+++ resources/views/frontend/dashboard/header.blade.php
@@ -1,11 +1,7 @@
-@php
-    $id = Auth::user()->id;
-    $profileData = App\Models\User::find($id);
-@endphp
 <nav class="navbar navbar-expand-lg navbar-light bg-light osahan-nav shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ route('index') }}"><img alt="logo"
-                src="{{asset('frontend/img/logo.png')}}"></a>
+                src="{{ asset('frontend/img/logo.png') }}"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -24,20 +20,41 @@
                         Restaurants
                     </a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <img alt="Generic placeholder image"
-                            src="{{ !empty($profileData->photo) ? url('upload/user_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
-                            class="nav-osahan-pic rounded-pill"> My Account
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow-sm border-0">
-                        <a class="dropdown-item" href="{{ route('dashboard') }}"><i class="icofont-home"></i>
-                            Dashboard</a>
-                        <a class="dropdown-item" href="{{ route('user.logout') }}"><i class="icofont-logout"></i>
-                            Logout</a>
-                    </div>
-                </li>
+                @auth
+                    @php
+                        $id = Auth::user()->id;
+                        $profileData = App\Models\User::find($id);
+                    @endphp
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            <img alt="Generic placeholder image"
+                                src="{{ !empty($profileData->photo) ? url('upload/user_images/' . $profileData->photo) : url('upload/no_image.jpg') }}"
+                                class="nav-osahan-pic rounded-pill">
+                            My Account
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow-sm border-0">
+                            <a class="dropdown-item" href="{{ route('dashboard') }}"><i class="icofont-home"></i>
+                                Dashboard</a>
+                            <a class="dropdown-item" href="{{ route('user.logout') }}"><i class="icofont-logout"></i>
+                                Logout</a>
+                        </div>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}" role="button" aria-haspopup="true"
+                            aria-expanded="false">
+                            Login
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}" role="button" aria-haspopup="true"
+                            aria-expanded="false">
+                            Register
+                        </a>
+                    </li>
+                @endauth
                 <li class="nav-item dropdown dropdown-cart">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
